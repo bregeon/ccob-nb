2a423dbb70544cc6ec02fdb67daff78c113cc2b8
Johan Bregeon <johan.bregeon@gmail.com>
2019-07-08 16:48:25 +0200

    working

--- diode/diode.py
+++ diode/diode.py
@@ -43,6 +43,7 @@ def plot_diode(wl_dict):
     plt.xlabel(r'$\lambda$ (nm)')
     plt.ylabel('Conversion')
     plt.legend()
+    plt.grid(linestyle='--', linewidth=0.5)
     plt.show()
 
 def get_value(wl, wl_dict):
@@ -54,10 +55,9 @@ def get_value(wl, wl_dict):
    # No, so make a linear interpolation
     wl_list = list(wl_dict.keys())
     wl_list.sort()
-    pwr = list()
-    for wl in wl_list:
-        pwr.append(wl_dict[wl][0])
-
+    pow = list()
+    for one in wl_list:
+        pow.append(wl_dict[one][0])
     x2 = list(np.copy(wl_list))
     x3 = list(np.zeros(len(x2)))
     for i in range(len(x2)):
@@ -65,12 +65,12 @@ def get_value(wl, wl_dict):
         x3[i] = abs(x3[i])
     a = min(x3)
     idxa = x3.index(a)
-    ya = pwr[idxa]
+    ya = pow[idxa]
     xa = x2[idxa]
     x3[idxa] = 2000
     b = np.min(np.abs(x3))
     idxb = x3.index(b)
-    yb = pwr[idxb]
+    yb = pow[idxb]
     xb = x2[idxb]
     t = ya-((yb-ya)/(xb-xa))*xa
     return ((yb-ya)/(xb-xa))*wl + t
@@ -81,12 +81,13 @@ if __name__ == "__main__":
     wl_dict = read_data()
 
     # print table back for crosscheck
-    print_diode(wl_dict)
+    # print_diode(wl_dict)
 
     # Test get_value with and with out interpolation
-    for wl in range (600, 611):
-        pwr = get_value(wl, wl_dict)
-        print('{:<8} {:<10}'.format(wl, pwr))
+    print('Testing get_value for a number of wavelengths')
+    for wl in range(600, 611):
+        val = get_value(wl, wl_dict)
+        print('{:<8} {:<10}'.format(wl, val))
 
     # plot diode response
     plot_diode(wl_dict)
