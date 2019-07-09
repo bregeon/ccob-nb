bf91b22acb094cb97bdfa76583471935fb120739
Johan Bregeon <johan.bregeon@gmail.com>
2019-07-09 16:34:20 +0200

    calibration lines

---
+++ data/Hg2_calibration_lamp.py
@@ -0,0 +1,3 @@
+# A list of all atomic line of the Hg2 calibration lamp
+calib_lines = [296, 302, 313, 334, 365, 404, 435, 696, 706, 714,
+               738, 750, 763, 772, 800, 826, 842, 852, 866, 912]
