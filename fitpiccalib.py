a287aa6c912522e311edee2adf8fa11d988f2478
Johan Bregeon <johan.bregeon@gmail.com>
2019-07-09 14:52:22 +0200

    new data

--- fitpiccalib.py
+++ fitpiccalib.py
@@ -14,7 +14,9 @@ from lmfit.models import ExponentialModel, GaussianModel
 # content = open('HDX004411_10-13-13-243.txt').readlines()[14:]
 # content = open('HDX004411_14-25-09-210.txt').readlines()[14:]
 # content = open('HDX004411_15-21-26-328.txt').readlines()[14:]
-content = open('data/HDX004411_15-26-27-108.txt').readlines()[14:] # 20/20 um, s=1.3
+# content = open('data/HDX004411_15-26-27-108.txt').readlines()[14:] # 20/20 um, s=1.3
+# content = open('data/HDX004411_10-36-24-949.txt').readlines()[14:] # 900 nm, 7 um, s=1.3
+content = open('data/HDX004411_11-07-07-346.txt').readlines()[14:] # 400 nm, 7 um, s=1.4
 
 # content = open('spectre_lampe_calib_100ms.txt').readlines()[14:]
 
@@ -30,7 +32,7 @@ for line in content:
 np_x = np.array(all_x, 'float')
 np_y = np.array(all_y, 'float')
 
-wl = 900
+wl = 400
 i_min = len(np_x)-sum(np_x>wl) - 10
 i_max = len(np_x)-sum(np_x>wl) + 10
 
