abe8da5acc5a4cabc7df1917c83ad63be3cd975c
Johan Bregeon <johan.bregeon@gmail.com>
2019-07-09 16:25:57 +0200

    fit one peak

---
+++ utils/tools.py
@@ -0,0 +1,48 @@
+"""
+Created on Tue 09 Jul 2019 03:30:31 PM CEST
+
+@author: Johan Bregeon
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+from lmfit.models import ExponentialModel, GaussianModel
+
+def fit_peak(wl, np_x, np_y, plot_components=False):
+    ''' Fit one peak
+        wl wavelength
+        x axis values as a numpy array
+        y axis values as a numpy array
+    '''
+    i_min = len(np_x)-sum(np_x>wl) - 10
+    i_max = len(np_x)-sum(np_x>wl) + 10
+
+    x = np_x[i_min:i_max]
+    y = np_y[i_min:i_max]
+
+    exp_mod = ExponentialModel(prefix='exp_')
+    pars = exp_mod.guess(y, x=x)
+
+    gauss1 = GaussianModel(prefix='g1_')
+    pars.update(gauss1.make_params())
+    pars['g1_center'].set(wl, min=wl-5, max=wl+5)
+    pars['g1_sigma'].set(1, min=0.01)
+    pars['g1_amplitude'].set(50000, min=10)
+
+    mod = gauss1 + exp_mod
+    init = mod.eval(pars, x=x)
+    out = mod.fit(y, pars, x=x)
+    print(out.fit_report(min_correl=0.5))
+
+    plt.figure()
+    plt.plot(x, y, 'b+--',label='data')
+    plt.plot(x, init, 'k--')
+    plt.plot(x, out.best_fit, 'r',label='fit')
+    plt.legend()
+
+    if plot_components:
+        comps = out.eval_components(x=x)
+        plt.plot(x, comps['g1_'], 'b--')
+        plt.plot(x, comps['exp_'], 'k--')
+
+    plt.show()
