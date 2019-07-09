120f32a2f0771d222b31690958926c3da06c19ec
Johan Bregeon <johan.bregeon@gmail.com>
2019-07-09 16:25:35 +0200

    move fit in tools

--- fitpiccalib.py
+++ fitpiccalib.py
@@ -2,12 +2,12 @@
 """
 Created on Mon Jul  1 10:23:22 2019
 
-@author: andres
+@author: andres, bregeon
 """
 
-import matplotlib.pyplot as plt
+import sys
 import numpy as np
-from lmfit.models import ExponentialModel, GaussianModel
+from utils.tools import fit_peak
 
 #spectre_lampe_calib_100ms.dat
 #dat = np.loadtxt('spectre_lampe_calib_10ms.txt')
@@ -33,55 +33,8 @@ np_x = np.array(all_x, 'float')
 np_y = np.array(all_y, 'float')
 
 wl = 400
-i_min = len(np_x)-sum(np_x>wl) - 10
-i_max = len(np_x)-sum(np_x>wl) + 10
 
-x = np_x[i_min:i_max]
-y = np_y[i_min:i_max]
-# x = dat[:, 0]
-# y = dat[:, 1]
-
-
-exp_mod = ExponentialModel(prefix='exp_')
-pars = exp_mod.guess(y, x=x)
-
-gauss1 = GaussianModel(prefix='g1_')
-pars.update(gauss1.make_params())
-
-
-
-pars['g1_center'].set(wl, min=wl-5, max=wl+5)
-pars['g1_sigma'].set(1, min=0.01)
-pars['g1_amplitude'].set(50000, min=10)
-
-
-
-mod = gauss1 + exp_mod
-
-init = mod.eval(pars, x=x)
-
-
-out = mod.fit(y, pars, x=x)
-print(out.fit_report(min_correl=0.5))
-
-
-plot_components = False
-
-
-plt.figure()
-
-plt.plot(x, y, 'b+--',label='data')
-plt.plot(x, init, 'k--')
-plt.plot(x, out.best_fit, 'r',label='fit')
-plt.legend()
-
-if plot_components:
-    comps = out.eval_components(x=x)
-    plt.plot(x, comps['g1_'], 'b--')
-    plt.plot(x, comps['exp_'], 'k--')
-
-plt.show()
-
+fit_peak(400, np_x, np_y)
 
 
 
