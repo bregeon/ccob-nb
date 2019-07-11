831e828469b66a44bd222433b48e5881066413e7
Johan Bregeon <johan.bregeon@gmail.com>
2019-07-11 15:34:29 +0200

    improve and simplify fit plotting

--- utils/tools.py
+++ utils/tools.py
@@ -20,6 +20,7 @@ def fit_peak(wl, np_x, np_y, with_plot=True, plot_components=False):
     i_max = len(np_x)-sum(np_x>wl) + 10
 
     x = np_x[i_min:i_max]
+    many_x = np.arange(x[0], x[-1], 0.01)
     y = np_y[i_min:i_max]
 
     exp_mod = ExponentialModel(prefix='exp_')
@@ -32,26 +33,37 @@ def fit_peak(wl, np_x, np_y, with_plot=True, plot_components=False):
     pars['g1_amplitude'].set(50000, min=10)
 
     mod = gauss1 + exp_mod
-    init = mod.eval(pars, x=x)
+    # init = mod.eval(pars, x=many_x)
     out = mod.fit(y, pars, x=x)
+    comps = out.eval_components(x=many_x)
     print(out.fit_report(min_correl=0.5))
+    print('Gaussian line model')
+    print('Mean={:.2f}+/-{:.2f}\tFWHM={:.2f}+/-{:.2f}'.format(
+           out.params['g1_center'].value, out.params['g1_center'].stderr,
+           out.params['g1_fwhm'].value, out.params['g1_fwhm'].stderr))
 
     if with_plot:
-        plt.figure()
-        plt.plot(x, y, 'b+--',label='data')
-        plt.plot(x, init, 'k--')
-        plt.plot(x, out.best_fit, 'r',label='fit')
-        plt.legend()
-        if plot_components:
-            comps = out.eval_components(x=x)
-            plt.plot(x, comps['g1_'], 'b--')
-            plt.plot(x, comps['exp_'], 'k--')
+        fig = plt.figure()
+        out.plot(fig=fig, numpoints=100)
         plt.show()
-        
+
+    # if with_plot:
+    #     plt.figure()
+    #     plt.plot(x, y, 'b+',label='data')
+    #     # plt.plot(many_x, init, 'k--')
+    #     # plt.plot(x, out.best_fit, 'r',label='fit')
+    #     full_model = mod.eval(out.params, x=many_x)
+    #     plt.plot(many_x, full_model, 'r--',label='model')
+    #     plt.legend()
+    #     if plot_components:
+    #         plt.plot(many_x , comps['g1_'], 'b--')
+    #         plt.plot(many_x, comps['exp_'], 'k--')
+    #     plt.show()
+    #
     return out
 
 
 if __name__ == "__main__":
     # get data from text file and fit
     np_x, np_y = read_data('data/HDX004411_11-07-07-346.txt')
-    fit_peak(400, np_x, np_y, plot_components=False)
+    out = fit_peak(400, np_x, np_y, plot_components=False)
