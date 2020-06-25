634a5b6d70990fcc6f5769539dc87c8b0568acc6
Johan Bregeon <johan.bregeon@gmail.com>
2020-06-25 15:47:06 +0200

    read and plot picoamp data from csv

---
+++ scripts/plot_pico.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu June  25 12:27 2020
+
+@authors: Johan Bregeon (bregeon@in2p3.fr)
+"""
+
+import sys
+from utils.tools import fit_peak
+from pico.pico import read_data, plot_time_series
+
+
+if __name__ == "__main__":
+    # give a file path and wavelength
+    if len(sys.argv[1:]) < 1:
+        print('Please give a file path to a spectrum')
+        print('Try:\n \
+        python scripts/plot_time_series.py data/ReadPico.csv')
+        sys.exit(1)
+    file_path = sys.argv[1]
+    
+    # read data from file
+    time_series = read_data(file_path)
+    # show full time series
+    plot_time_series(time_series, file_path)
