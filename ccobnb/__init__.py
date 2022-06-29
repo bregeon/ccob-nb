aeefd66906ef3c9d539a9925b770e5462d34198a
Johan Bregeon <johan.bregeon@gmail.com>
2022-06-29 14:33:41 +0200

    init with version

---
+++ ccobnb/__init__.py
@@ -0,0 +1,6 @@
+"""ccob-nb is a library of functions to analyze the Rubin LSST CCOB narrow beam calibration data"""
+
+try:
+    from ._version import version
+except:  #pragma: no cover
+    version = "unknown"
