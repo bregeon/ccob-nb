b5c0feaa6fcba1ee58ac03b0253dfb63a637148f
Johan Bregeon <johan.bregeon@gmail.com>
2022-06-29 14:47:21 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name="ccob_nb",
+    author="Johan Bregeon",
+    author_email="bregeon@in2p3.fr",
+    url = "https://github.com/bregeon/ccob-nb",
+    packages=["ccobnb"],
+    description="Analysis of Rubin telescope CCOB NB system",
+    setup_requires=['setuptools_scm'],
+    long_description=open("README.md").read(),
+    package_data={"": ["README.md", "LICENSE"]},
+    use_scm_version={"write_to":"ccobnb/_version.py"},
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 1 - Beta",
+        "License :: OSI Approved :: GPL License",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        ],
+    install_requires=["matplotlib",
+                      "numpy",
+                      "scipy",
+                      "setuptools_scm"]
+)
